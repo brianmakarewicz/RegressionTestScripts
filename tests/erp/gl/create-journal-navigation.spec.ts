fa1a4d64ee8d0cbe05f5c0c0553249149c61b8f8
Bryan Viera <bryanviera08@gmail.com>
2026-07-08 11:49:12 -0400

    Stabilize Oracle login readiness check

--- tests/erp/gl/create-journal-navigation.spec.ts
+++ tests/erp/gl/create-journal-navigation.spec.ts
@@ -1,14 +1,11 @@
 import { test } from '@playwright/test';
-import { FusionLoginPage } from '../../../pages/common/fusion-login.page';
+import { AuthenticationWorkflow } from '../../../workflows/authentication.workflow';
 import { FusionNavigatorPage } from '../../../pages/common/fusion-navigator.page';
 
 test('user can navigate to the Create Journal page', async ({ page }) => {
-  const loginPage = new FusionLoginPage(page);
+  const authentication = new AuthenticationWorkflow(page);
   const navigatorPage = new FusionNavigatorPage(page);
 
-  await loginPage.goto();
-  await loginPage.login();
-  await loginPage.waitForFusionHomePage();
+  await authentication.login();
   await navigatorPage.goToCreateJournalPage();
-  
 });
