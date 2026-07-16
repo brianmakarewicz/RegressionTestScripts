d6ab448abf05dfb2f7ec72b54c86a1411949acd0
Taylor Wood <82664339+taylorwood2@users.noreply.github.com>
2026-07-16 17:27:03 -0400

    Update validate-approve-invoice.spec.ts
    Removed commented code that isn't needed anymore

--- tests/erp/ap/validate-approve-invoice.spec.ts
+++ tests/erp/ap/validate-approve-invoice.spec.ts
@@ -34,16 +34,6 @@ test('navigate to created invoice in Oracle Fusion', async ({ page }) => {
   await authentication.login();
   await navigatorPage.goToAPInvoice(createdInvoice.invoiceNumber);
 
-  // await page.getByRole('link', { name: 'Navigator' }).click();
-  // await page.getByTitle('Payables', { exact: true }).click();
-  // await page.getByRole('link', { name: 'Invoices' }).click();
-  // await page.getByRole('link', { name: 'Tasks' }).click();
-  // await page.getByRole('link', { name: /manage invoices/i }).click();
-  // await page.getByRole('textbox', { name: 'Invoice Number' }).click();
-  // await page.getByRole('textbox', { name: 'Invoice Number' }).fill(createdInvoice.invoiceNumber);
-  // await page.getByRole('button', { name: 'Search', exact: true }).click();
-  // await page.getByRole('link', { name: createdInvoice.invoiceNumber }).click();
-  // await expect(page.getByRole('heading', { name: 'Invoice Details' })).toBeVisible({timeout: 5 * 60 * 1000 });
   await page.getByRole('link', { name: 'Actions', exact: true }).click();
   await page.getByText('Validate', { exact: true }).click();
   await expect(page.locator('td').filter({ hasText: /^Validated$/ }).first()).toBeVisible();
