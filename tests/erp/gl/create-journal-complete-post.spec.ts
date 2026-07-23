70af6d04c6f9b4a859a253611ec9bee9e854352f
Bryan Viera <72482373+bryanviera22@users.noreply.github.com>
2026-07-23 18:03:38 -0400

    Load Create Journal tests from client-specific data (#16)
    * Add reusable Create Journal test data model
    
    * Load Create Journal tests from client data

--- tests/erp/gl/create-journal-complete-post.spec.ts
+++ tests/erp/gl/create-journal-complete-post.spec.ts
@@ -1,11 +1,25 @@
+import path from "node:path";
 import { test } from "@playwright/test";
+import { env } from "../../../config/environment";
 import { AuthenticationWorkflow } from "../../../workflows/authentication.workflow";
 import { FusionNavigatorPage } from "../../../pages/common/fusion-navigator.page";
 import { CreateJournalPage } from "../../../pages/erp/gl/create-journal.page";
+import { loadCreateJournalData } from "../../../utils/test-data/load-create-journal-data";
 
-test("user can create, complete, and post a journal", async ({
-  page,
-}) => {
+test("user can create, complete, and post a journal", async ({ page }) => {
+  test.setTimeout(180_000);
+
+  const journalDataFilePath = path.join(
+    "test-data",
+    "clients",
+    env.clientAlias,
+    env.environment,
+    "gl",
+    "create-journal.json",
+  );
+  const journalData = loadCreateJournalData(journalDataFilePath);
+  const journalBatchName = `${journalData.batchNamePrefix}_${Date.now()}`;
+
   const authentication = new AuthenticationWorkflow(page);
   const navigatorPage = new FusionNavigatorPage(page);
   const createJournalPage = new CreateJournalPage(page);
@@ -14,33 +28,36 @@ test("user can create, complete, and post a journal", async ({
   await navigatorPage.goToCreateJournalPage();
 
   await createJournalPage.waitForCreateJournalPage();
-  await createJournalPage.enterJournalBatchName(`TEST_BATCH_${Date.now()}`);
-  await createJournalPage.enterBatchDescription("TEST_BATCH_DESCRIPTION_001");
-  await createJournalPage.selectBalanceType("Actual");
-  //await createJournalPage.selectBalanceType("Encumbrance"); //Might use in future tests for encumbrance journals
-  await createJournalPage.selectAccountingPeriod("Oct-25");
+  await createJournalPage.enterJournalBatchName(journalBatchName);
+  await createJournalPage.enterBatchDescription(journalData.batchDescription);
+  await createJournalPage.selectBalanceType(journalData.balanceType);
+  await createJournalPage.selectAccountingPeriod(journalData.accountingPeriod);
   await createJournalPage.chooseAttachmentFile(
-    "test-data/attachments/TestFile.example.txt",
+    journalData.attachmentFilePath,
   );
-  await createJournalPage.selectLedger("FAYNC Ledger");
-  await createJournalPage.selectCategory("Adjustment");
+  await createJournalPage.selectLedger(journalData.ledger);
+  await createJournalPage.selectCategory(journalData.category);
 
-  await createJournalPage.enterJournalLineAccount(
-    1,
-    "026.00.301.0000.6421101.00000000.00000",
-  );
-  await createJournalPage.enterJournalLineDebit(1, "5");
-  await createJournalPage.enterJournalLineDescription(1, "Description_1");
+  for (const [index, line] of journalData.lines.entries()) {
+    const lineNumber = index + 1;
 
-  await createJournalPage.enterJournalLineAccount(
-    2,
-    "026.00.000.0000.1130421.00000000.00000",
-  );
-  await createJournalPage.enterJournalLineCredit(2, "5");
-  await createJournalPage.enterJournalLineDescription(2, "Description_2");
+    await createJournalPage.enterJournalLineAccount(lineNumber, line.account);
+
+    if (line.debit !== undefined) {
+      await createJournalPage.enterJournalLineDebit(lineNumber, line.debit);
+    }
+
+    if (line.credit !== undefined) {
+      await createJournalPage.enterJournalLineCredit(lineNumber, line.credit);
+    }
+
+    await createJournalPage.enterJournalLineDescription(
+      lineNumber,
+      line.description,
+    );
+  }
 
   await createJournalPage.save();
   await createJournalPage.completeJournal();
   await createJournalPage.postJournal();
-
 });
